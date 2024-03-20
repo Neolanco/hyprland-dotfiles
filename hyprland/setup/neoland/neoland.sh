1d5cad3d84bf7c4cf7e389b8b6c8806447f416d5
neolanco <www.leon098@gmail.com>
2024-03-20 15:16:13 +0100

    20.03.24 2

--- hyprland/setup/neoland/neoland.sh
+++ hyprland/setup/neoland/neoland.sh
@@ -3,8 +3,8 @@ sudo dnf install -y hyprland swaybg
 sudo apk add hyprland swaybg
 
 # nice stuff
-sudo dnf install -y fuzzel python-dasbus polkit-gnome wev kde-connect kde-connect-nautilus waybar pamixer wlogout pavucontrol cliphist wl-clipboard swaylock brillo
-sudo apk add fuzzel polkit-gnome wev waybar pamixer wlogout pavucontrol cliphist wl-clipboard swaylock tofi playerctl brightnessctl pavucontrol brillo #python-dasbus kde-connect kde-connect-nautilus
+sudo dnf install -y network-manager-applet fuzzel python-dasbus polkit-gnome wev kde-connect kde-connect-nautilus waybar pamixer wlogout pavucontrol cliphist wl-clipboard swaylock brillo
+sudo apk add network-manager-applet fuzzel polkit-gnome wev waybar pamixer wlogout pavucontrol cliphist wl-clipboard swaylock tofi playerctl brightnessctl pavucontrol brillo #python-dasbus kde-connect kde-connect-nautilus
 
 #hyprpm
 sudo dnf install -y cpio meson cmake g++
